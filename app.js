66ab321ab600d684879aff8005a49d3f1d2b5007
melakhvision <45154979+melakhvision@users.noreply.github.com>
2022-10-19 20:08:08 +0000

    DOCKERIZE NODE BACKEND

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const conn = require("./models/db");
+const { json } = require("express");
+var corsOptions = {
+  origin: "*",
+};
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+
+app.post("/adduser", (req, res) => {
+  const values = [req.body.firstName, req.body.lastName];
+  conn.query(
+    "INSERT INTO users (firstName, lastName) VALUES(?)",
+    [values],
+    function (err, data, fields) {
+      if (err) return res.json({ message: err });
+      res.status(201).json({
+        status: "success",
+        message: "User has been added Successfully",
+      });
+    }
+  );
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
