72d859eea484e3407d981f74330b95bd9d678589
Codekhalipha <45154979+melakhvision@users.noreply.github.com>
2022-12-03 19:48:17 -0500

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: process.env.HOST || "http://dev.melakhactu.com",
+    HOST: process.env.HOST || "dev.melakhactu.com",
     USER: process.env.MYSQL_USER || 'root',
     PASSWORD: process.env.MYSQL_PASSWORD || 'winkerdev630',
     DB: process.env.MYSQL_DB_NAME || 'devnode',
